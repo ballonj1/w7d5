ae65963821b08da28188a4ed9dff554fe6d82c7a
App Academy Student <student@appacademy.io>
2017-05-12 17:05:12 -0700

    Create actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,14 +1,26 @@
 import * as APIUtil from '../util/session_api_util'
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 
 export const login = (user) => (dispatch) => ({
-  APIUtil.login(user).then((session) => dispatch(receiveCurrentUser(session)))
-})
+  APIUtil.login(user).then((currentUser) => dispatch(receiveCurrentUser(currentUser)))
+});
 
 export const receiveCurrentUser = (session) => ({
   type: RECEIVE_CURRENT_USER,
   session
-})
+});
 
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='John Ballon'; GIT_AUTHOR_EMAIL='ballonj1@tcnj.edu'; GIT_COMMITTER_NAME='John Ballon'; GIT_COMMITTER_EMAIL='ballonj1@tcnj.edu';" HEAD
+export const logout = () => (dispatch) => ({
+  APIUtil.logout().then.((currentUser) => dispatch(receiveCurrentUser(currentUser))
+});
+
+export const signup = (user) => (dispatch) => ({
+  APIUtil.signup(user).then((currentUser => dispatch(receiveCurrentUser(currentUser))))
+});
+
+export const receiveErrors = (errors) => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
